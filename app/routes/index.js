0a3c3d0f1cc7c7327913a5d14b5a185237780f18
ssnau <korige@gmail.com>
2013-04-02 10:44:01 +0800

    revise 404 audio

--- app/routes/index.js
+++ app/routes/index.js
@@ -20,6 +20,6 @@ module.exports = function(app) {
     app.get("/voice/:word", function(req, res) {
         var audio = helper.getWordVoice(req.params.word, app.VOICE_ROOT);
         audio && res.send(audio);
-        res.send(404);
+        !audio && res.send(404);
     })
 }
